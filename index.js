90188971185a43416a317e014f56b1f821be63e9
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-10-27 22:48:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { StateProvider } from './StateProvider';
-import reducer, { initialState } from './reducer';
+const{onRequest}=require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")  ("sk_test_51QA4UXRty9CXj9c8z3WqEEIli0RalHZ8n0VZpsrwXSQFmKgoANFE1fTAUFq2pdv0OnlIdcXpRyOibiAuO7ZfWA8l00rtkElXEO");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-   <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-   </StateProvider>
-  </React.StrictMode>
-);
+const app = express();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(cors({origin: true}));
+app.use(express.json());
+
+app.get("/", (request, response)=>response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  logger.log("Payment Request Received BOOM !! for this Amount ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "inr",
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+exports.api = onRequest(app); // Use onRequest from v2
